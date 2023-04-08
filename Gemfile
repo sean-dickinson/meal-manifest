d30259b5ab5f172cc53592e9d197e7caa8966784
Sean Dickinson <contact@seandickinson.dev>
2023-04-08 11:46:56 -0400

    feat: add vite with stimulus

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ end
 gem "paranoia"
 gem "devise"
 gem 'acts-as-taggable-on', '~> 9.0'
+gem 'vite_rails'
 
 group :development, :test do
   gem "capybara"
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,9 @@
-// Import and register all your controllers from the importmap under controllers/*
+import { Application } from "stimulus"
+import { registerControllers } from 'stimulus-vite-helpers'
 
-import { application } from "controllers/application"
+// Start the Stimulus application.
+const application = Application.start()
 
-// Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
-
-// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
+// Controller files must be named *_controller.js.
+const controllers  = import.meta.globEager('./**/*_controller.js')
+registerControllers(application, controllers)
