6b39a302821cc9a0aa705004f85bedf268902568
Sean Dickinson <contact@seandickinson.dev>
2023-04-09 20:51:12 -0400

    feat: add user scoped tags

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable
 
   has_many :meals, dependent: :destroy
+
+  # Get all tags for meals belonging to this user
+  # @return [ActiveRecord::Relation<ActsAsTaggableOn::Tag>]
+  def meal_tags
+    ActsAsTaggableOn::Tag
+      .where(taggings: {taggable_type: "Meal", context: "tags"})
+      .joins(:taggings)
+      .joins("INNER JOIN meals on taggable_id = meals.id")
+      .where("meals.user_id = ?", id)
+  end
 end
