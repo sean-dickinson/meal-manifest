332d483624e1affb70fdf8820f7f5c08d39b3fd2
Sean Dickinson <contact@seandickinson.dev>
2023-05-06 10:35:34 -0400

    add tailwind and postcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        './app/views/**/*.{html,html.erb,erb}',
+        './app/views/devise/**/*.{html,html.erb,erb}',
+        './app/frontend/components/**/*.{vue,js,ts}',
+        './app/frontend/**/*.{js,ts}',
+        './app/**/*.{vue,js,ts}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
